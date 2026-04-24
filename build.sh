44947e93582274a680df0891744b04ba39d58067
Justlinuxyourself <alithelinuxuser@gmail.com>
2026-04-24 14:40:39 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+fasm bootloader.asm bootloader.bin
+
+gcc -m32 -ffreestanding -fno-stack-protector -O0 -c ata.c -o ata.o
+gcc -m32 -ffreestanding -fno-stack-protector -O0 -c cpu.c -o cpu.o
+gcc -m32 -ffreestanding -fno-stack-protector -O0 -c input.c -o input.o
+gcc -m32 -ffreestanding -fno-stack-protector -O0 -c io.c -o io.o
+gcc -m32 -ffreestanding -fno-stack-protector -O0 -c larpfs.c -o larpfs.o
+gcc -m32 -ffreestanding -fno-stack-protector -O0 -c lib.c -o lib.o
+gcc -m32 -ffreestanding -fno-stack-protector -O0 -c main.c -o main.o
+gcc -m32 -ffreestanding -fno-stack-protector -O0 -c shell.c -o shell.o
+gcc -m32 -ffreestanding -fno-stack-protector -O0 -c vga.c -o vga.o
+
+ld -m elf_i386 -T linker.ld --oformat binary -o kernel.bin main.o ata.o cpu.o input.o io.o larpfs.o lib.o shell.o vga.o
+
+dd if=/dev/zero of=larpos.img bs=512 count=2880
+dd if=bootloader.bin of=larpos.img conv=notrunc
+dd if=kernel.bin of=larpos.img seek=1 conv=notrunc
